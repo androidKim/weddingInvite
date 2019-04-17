6871df9eb73b81c50d998916401db4762064d2b5
taejun <taejun@MacBook-Pro.local>
2019-04-17 15:00:11 +0900

    dummy commit

--- src/main/java/com/midasgo/weddinginvite/controller/MainController.java
+++ src/main/java/com/midasgo/weddinginvite/controller/MainController.java
@@ -24,6 +24,7 @@ public class MainController
     }
     //-----------------------------------------------------------------------------
     //
+    
     @RequestMapping(value = "/main")
     public ModelAndView main(Model model, HttpServletRequest request, HttpServletResponse response) 
     {	    	
