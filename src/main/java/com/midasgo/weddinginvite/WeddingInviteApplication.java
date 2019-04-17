af1667bb778ecd5334fa9984219d8aad8e1283f7
taejun <taejun@MacBook-Pro.local>
2019-04-17 14:47:17 +0900

    first commit

---
+++ src/main/java/com/midasgo/weddinginvite/WeddingInviteApplication.java
@@ -0,0 +1,13 @@
+package com.midasgo.weddinginvite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingInviteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingInviteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/midasgo/weddinginvite/controller/MainController.java
@@ -0,0 +1,33 @@
+package com.midasgo.weddinginvite.controller;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class MainController
+{     
+	/******************************* Define *******************************/
+	
+	/******************************* Member *******************************/
+	    /******************************* User Function *******************************/
+    //-----------------------------------------------------------------------------
+    //
+    @RequestMapping(value = "/")
+    public String redirect(Model model, HttpServletRequest request, HttpServletResponse response) 
+    {	    	
+		return "redirect:/main";
+    }
+    //-----------------------------------------------------------------------------
+    //
+    @RequestMapping(value = "/main")
+    public ModelAndView main(Model model, HttpServletRequest request, HttpServletResponse response) 
+    {	    	
+    	ModelAndView mav = new ModelAndView();
+		mav.setViewName("main");
+		return mav;
+    } 
+}
